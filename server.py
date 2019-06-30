ed13015944e46ffe4d0a1059fb8b9826baf38a78
memset0 <memset0@outlook.com>
2019-06-30 13:42:31 +0800

    fix: bzoj support

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def add_problem():
             id = re.findall(r'[0-9]+', it)
             if len(id) == 1:
                 problem.append(Problem('LOJ #' + id[0]))
-        elif 'BZOJ' in it or 'bzoj' in it or lydsy in it:
+        elif 'BZOJ' in it or 'bzoj' in it or 'lydsy' in it:
             id = re.findall(r'[0-9]+', it)
             if len(id) == 1:
                 problem.append(Problem('BZOJ #' + id[0]))
